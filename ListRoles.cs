37eb68f0a88bcc3324c72af921ace174bf3990e3
nthanhnhan-dev <nguyenthanhnhan24092002@gmail.com>
2023-03-31 15:20:19 +0700

    add listroles

---
+++ ListRoles.cs
@@ -0,0 +1,13 @@
+namespace Myfirst;
+
+public partial class ListRoles : Form
+{
+    private Button? selectedButton;
+   
+    public ListRoles()
+    {
+        InitializeComponent();
+        selectedButton = null;
+    }
+    
+}
