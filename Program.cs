2e4476ba9ba86c524bffdb759c1d5fdde561bf6a
HoaiMinh <phamlehoaiminh2014@gmail.com>
2023-03-31 12:42:48 +0700

    them file UserEdit Privileges

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ static class Program
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        //Application.Run(new Login());
-        Application.Run(new MainWindow());
+        Application.Run(new Login());
+        // Application.Run(new MainWindow());
        //Application.Run(new ListOfUsers());
         //Application.Run(new UserPrivileges());
     }    
